d7ae3ccbadd71fae57c167540c8f92f21bef86de
Andre Landgraf <andre.timo.landgraf@gmail.com>
2024-11-19 18:52:07 -0800

    remove single fetch (debug failing img requests)

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -7,10 +7,8 @@ export default defineConfig({
   plugins: [
     remix({
       future: {
-        v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
     }),
