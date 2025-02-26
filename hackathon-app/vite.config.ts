59ae490479dca3805e62006fc9812b0dfd8ef601
andrelandgraf <andre.timo.landgraf@gmail.com>
2025-02-26 11:56:01 -0800

    hackathon voting app

---
+++ hackathon-app/vite.config.ts
@@ -0,0 +1,11 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(({ command }) => ({
+  ssr: {
+    noExternal: command === "build" ? true : undefined,
+  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+}));
---
+++ sync/README.md
@@ -0,0 +1,3 @@
+# Electric SQL Sync Server
+
+Electric SQL sync server hosted on Fly.io for the website app. Added as an experiment only.
