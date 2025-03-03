7ef8bfe8fb75084eb6249f2f29fa4164ad5edee6
Morel Sébastien <morel.seb@gmail.com>
2025-03-02 18:37:09 -0800

    feat: add info in speakers in the api

--- website/app/routes/api.speakers.ts
+++ website/app/routes/api.speakers.ts
@@ -4,6 +4,9 @@ export async function loader({ context }: Route.ActionArgs) {
     const speakersWithTalks = await context.queryClient.getSpeakersWithTalks();
     const data = speakersWithTalks.speakers.map((speaker) => ({
         name: speaker.name,
+        title: speaker.title,
+        link: speaker.socials.linkedinUrl || speaker.socials.twitterUrl || speaker.socials.blueskyUrl,
+
     }));
     return Response.json({ success: true, data });
 }
