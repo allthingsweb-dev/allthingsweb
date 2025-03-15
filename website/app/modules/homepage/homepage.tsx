e253dedd78e9a867eef0b6590a591840b4b17506
Andre Landgraf <andre.timo.landgraf@gmail.com>
2025-03-14 23:56:08 -0700

    landing page images

--- website/app/modules/homepage/homepage.tsx
+++ website/app/modules/homepage/homepage.tsx
@@ -16,16 +16,17 @@ const imageIds = [
   "0ad3906b-ed44-4b06-adaa-59f249e7f134",
   "976a8737-ece8-41d0-a90c-89f6336f283f",
   "74e1169f-484a-4563-96c7-6a23257ca14d",
-  "8016e3dc-9b18-4dd1-aab4-d145d2bee6e7",
-  "a44a661a-9252-423c-95b3-20393c77580c",
-  "a4750122-a7b0-4d6d-b7cd-083d1a6e303e",
+  "1399f2e8-5726-4430-820c-de60337e4e62",
   "11a0af02-7d1f-4563-9b8e-d77baf917308",
+  "fefa5ff6-4dee-41ec-aa3c-7972aa84dbdb",
+  "a4750122-a7b0-4d6d-b7cd-083d1a6e303e",
+  "a44a661a-9252-423c-95b3-20393c77580c",
   "673345e6-5bfb-4122-9ab9-8e3f5f24ac93",
   "080f48d1-7eb4-43c9-9440-79f3131b0e49",
-  "fefa5ff6-4dee-41ec-aa3c-7972aa84dbdb",
   "07fc788e-b13d-498b-8914-9d195be4d6ae",
   "2b220343-569a-441f-acfc-a906b5fb97d0",
   "4d224238-6b78-4d5e-9d84-4ca2d03b91b7",
+  "8016e3dc-9b18-4dd1-aab4-d145d2bee6e7",
 ];
 
 export async function getPastEventImages({ db, s3Client }: Deps) {
